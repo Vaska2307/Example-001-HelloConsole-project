2943a15eff34edd5cf32cb88aaa129526f6fc403
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-05-02 01:49:13 +0300

    Lesson_1_Task_1

--- Lesson_1_Task_1/Program.cs
+++ Lesson_1_Task_1/Program.cs
@@ -1,4 +1,4 @@
-﻿int[,] matrix = new int[3, 5];// определили тип данных int [,], дали имя matrix, выделили память под
+﻿//int[,] matrix = new int[3, 5];// определили тип данных int [,], дали имя matrix, выделили память под
 //элементы массива new int[3, 5] 3 строки и 5 элементов в каждой строке.
 
 // Далее нам неоходимо заполнить двухмерный массив, тоесть обатиться к каждому его элементу и поместить
@@ -8,21 +8,53 @@
 // 3. У нас должно получиться ровно два (2) цикла. 1 Первый будет отвечать за проход по строкам 
 //(одномерные массивы). А 2 второй будет отвечать за проход по элементам каждого из этих одномерных 
 // массивов (столбцам).
-Random rnd = new Random();
-for (int i = 0; i < matrix.GetLength(0); i++)
+//Random rnd = new Random();
+//for (int i = 0; i < matrix.GetLength(0); i++)
+//{
+   // for (int j = 0; j < matrix.GetLength(1); j++)
+   // {
+       // matrix[i, j] = rnd.Next(1, 11);
+    //}
+//}
+
+//for (int i = 0; i < matrix.GetLength(0); i++)
+//{
+    //for (int j = 0; j < matrix.GetLength(1); j++)
+    //{
+       // Console.Write($"{matrix[i, j]} ");
+
+    //}
+   // Console.WriteLine();// это для того что бы вывести массив в двумерном виде 
+//}
+
+
+int[,] CreateMatrix(int rowCount, int columsCount)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        matrix[i, j] = rnd.Next(1, 11);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+
+        }
+        Console.WriteLine();
     }
 }
 
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        Console.Write($"{matrix[i, j]} ");
-        
-    }
-    Console.WriteLine();// это для того что бы вывести массив в двумерном виде 
-}
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
